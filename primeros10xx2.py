1ac90d7a39cff7c229ce66adac7407397d57ae72
LeandroT <deyvtenjo@gmail.com>
2021-04-15 17:47:00 -0500

    Completado el ejercicio de List Comprehensions

--- primeros10xx2.py
+++ primeros10xx2.py
@@ -13,12 +13,20 @@ def run():
     # print(cuadrados)
 
 # Con Comprehension
-    cuadrados = [i for i in range(0, 11) if i % 3 != 0]
-    print(cuadrados)
+    # cuadrados = [i**2 for i in range(0, 11) if i % 3 != 0]
+    # print(cuadrados)
 
 
+# Reto de la Clase
+# List Comprehensions de los muktupolis de 4, de 9 y de 6 
+# Que tengan maximo 5 cifras
 
 
+    reto = [i for i in range(1, 100000) if (i % 4 == 0 and i % 6 == 0 and i % 9 == 0)]
+
+    print(reto)
+
+    # Tambien podria evaluar el modulo de 36. Es el mcm de los 3
 
 
 if __name__ == '__main__':
