13333ca9bfe284356e45eaa5af346e24ce0b404f
LeandroT <deyvtenjo@gmail.com>
2021-04-15 15:34:44 -0500

    new: Ejercicio rapido de cuadrados en lista

---
+++ primeros10xx2.py
@@ -0,0 +1,16 @@
+def run():
+
+    cuadrados = []
+
+    for i in range(1, 11):
+        cuadrados.append(i**2)
+
+    for item in cuadrados:
+        print(item)
+
+
+
+
+
+if __name__ == '__main__':
+    run()
