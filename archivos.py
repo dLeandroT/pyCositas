68cd30351ce0d38f0adb35365921c573d1000b4a
LeandroT <deyvtenjo@gmail.com>
2021-04-23 13:21:12 -0500

    Ejercicio con manejo de archivos

---
+++ archivos.py
@@ -0,0 +1,27 @@
+def read():
+    numbers = []
+    with open("./numeros.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    print(numbers)
+
+
+def write():
+    names = ["Facundo", "Miguel", "Pepe", "Cristian", "Rocío"]
+    with open("./names.txt", "a", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+        
+
+
+
+def run():
+    write()
+
+
+
+
+
+if __name__ == '__main__':
+    run()
