790fca9c91efcf908ce2b508eeb58dd5c8c5ef76
LeandroT <deyvtenjo@gmail.com>
2021-04-23 18:47:29 -0500

    Agregue el gameloop y una funcion para elegir la palabra del archivo

--- juego_Ahorcado/hangman.py
+++ juego_Ahorcado/hangman.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 def imprimir(archivo):
     """
@@ -15,20 +16,36 @@ def draw_bienvenida():
     os.system("clear")
     imprimir("./bienvenida.txt")
     input("\n\n\nPresiona ENTER para continuar...")
+    print("_______________________________________")
         
     # Imprimir instrucciones
-    print("\n\n")
+    os.system("clear")
+    print("\n")
     imprimir("./instrucciones.txt")
     input("\n\nPresiona ENTER para continuar...")
 
 
+def choose_word():
+    with open("./palabras.txt", "r", encoding="utf-8") as f:
+        secret_words = [line.strip() for line in f]
+    return random.choice(secret_words)
 
     
+    
 
-
+#  Funcion Principal
 def run():
     draw_bienvenida()
+    secret_word = choose_word()
+    # Gamelooop 
+    is_gameover = False
+    while not is_gameover:
+        os.system("clear")
+        # Pendiente imprimir el estado del colgado
+        letra = input(" Ingresa una letra:  ")
+       
 
+        
 
 
 
