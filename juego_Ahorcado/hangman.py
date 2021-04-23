9bd52e1bf166e56003dbb771ce28004cede1311e
LeandroT <deyvtenjo@gmail.com>
2021-04-23 13:40:18 -0500

    Primer version de Bienvenida

--- juego_Ahorcado/hangman.py
+++ juego_Ahorcado/hangman.py
@@ -1,4 +1,18 @@
+import os
+
+
+def draw_bienvenida():
+    os.system("clear")
+    print("###################################################")
+    print("--- B I E N V E N I D O   A L   A H O R C A D O ---")
+    print("###################################################\n\n\n")
+    input("Presiona enter para empezar...\n\n\n")
+
+    
+
+
 def run():
+    draw_bienvenida()
 
 
 
