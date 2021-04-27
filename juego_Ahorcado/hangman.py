cd40b367abcc932731b61c7101522cf3cd1b604f
LeandroT <deyvtenjo@gmail.com>
2021-04-27 11:20:47 -0500

    Integracion de sistema de puntos tambien: Mejora en la impresion de pantalla final new: archivo con pantalla de derrota:

--- juego_Ahorcado/hangman.py
+++ juego_Ahorcado/hangman.py
@@ -25,22 +25,25 @@ def draw_bienvenida():
     input("\n\nPresiona ENTER para continuar...")
 
 
-def draw_progress(progress):
+def draw_progress(progress, score):
     os.system("clear")
-    print("### A D I V I N A   L A   P A L A B R A ###\n\n\n", end="\t\t")
+    print("### A D I V I N A   L A   P A L A B R A ###")
+    print("-------------------------------------------")
+    print(f"puntaje:   {score}")
+    print("-------------------------------------------\n\n\n", end="\t     ")
     for i in progress:
         print(i, end=" ")
     print("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
 
-def draw_victoria(secret_word):
+def draw_final(secret_word, score, archivo):
     os.system("clear")
+    print(f"puntaje final:  {score}")
     print("\n", end="\t\t")
     for letra in secret_word:
         print(letra, end=" ")
     print("")
-    imprimir("./victoria.txt")
-    
+    imprimir(archivo)
 
 
 def quitar_acento(letra):
@@ -75,30 +78,40 @@ def run():
     secret_word = choose_word()
     letras_ocultas = len(secret_word)
     hidden_word = ["_" for i in range(letras_ocultas)]
-    
+
+    # Para puntuacion 
+    score = 0
+    incorrectas = 0
+    limite_de_fallos = letras_ocultas * 3
 
     # Gamelooop 
     is_gameover = False
     while not is_gameover:
         os.system("clear")
-        draw_progress(hidden_word)
+        draw_progress(hidden_word, score)
         letra_ingresada = input("Ingresa una letra:  ").upper()
         quitar_acento(letra_ingresada)
         
         # Comprobar la Letra
+        control_puntos = letras_ocultas
         idx = 0
         for word in secret_word:
             if letra_ingresada == word:
                 hidden_word[idx] = letra_ingresada
                 letras_ocultas -= 1
+                score += 100
             idx += 1
+        if control_puntos == letras_ocultas:
+            incorrectas += 1
 
         # Comprobar si hay victoria
         if letras_ocultas == 0:
+            draw_final(secret_word, score, "./victoria.txt")
             is_gameover = True
-        
-    draw_victoria(secret_word)
-       
+        elif incorrectas == limite_de_fallos:
+            draw_final(secret_word, score, "./derrota.txt")
+            is_gameover = True
+
 
 
 
